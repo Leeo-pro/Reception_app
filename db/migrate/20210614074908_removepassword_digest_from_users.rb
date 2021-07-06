55dc31910c6cf564b4c156e234759d8de5188079
Leeo-pro <mineva.aug@gmail.com>
2021-07-06 08:34:49 +0000

    0706マイグレ削除

--- db/migrate/20210614074908_removepassword_digest_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemovepasswordDigestFromUsers < ActiveRecord::Migration[5.1]
-  def up
-    remove_column :users, :password_digest
-  end
-  
-  def down 
-    add_column :users, :password_digest, :string
-  end
-end
